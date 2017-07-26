46745a3d505c1b04b0ef53bec1c45c42bb678860
OldGhost366 <oldghost007@163.com>
2017-07-26 16:58:14 +0800

    增加类库接口代码

---
+++ HJPodTestLibA/Classes/HJPodTestA.h
@@ -0,0 +1,14 @@
+//
+//  HJPodTestB.h
+//  Pods
+//
+//  Created by j on 2017/7/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HJPodTestA : NSObject
+
++ (void)Start;
+@end
---
+++ HJPodTestLibA/Classes/HJPodTestA.m
@@ -0,0 +1,16 @@
+//
+//  HJPodTestB.m
+//  Pods
+//
+//  Created by j on 2017/7/26.
+//
+//
+
+#import "HJPodTestB.h"
+
+@implementation HJPodTestA
+
++ (void)Start{
+    NSLog(@"HJPodTestA");
+}
+@end
